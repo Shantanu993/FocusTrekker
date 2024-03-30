6a98f72c6fcdba71867f8f9019e3a0a718e43733
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-30 19:29:35 +0530

    Add PomodoroTimer component to Home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import PomodoroTimer from "@/components/PomodoroTimer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      Hello
-    </main>
+    <div>
+      <PomodoroTimer />
+    </div>
   );
 }
