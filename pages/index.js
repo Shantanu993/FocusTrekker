b6c7a54b9980ba22705f42a1fc07f03be45bd37c
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-30 22:56:37 +0530

    implement Settings component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import PomodoroTimer from "@/components/PomodoroTimer";
+import Settings from "@/components/Settings";
+import { useState } from "react";
+import SettingsContext from "@/components/SettingsContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
   return (
-    <div>
-      <PomodoroTimer />
-    </div>
+    <main>
+      <SettingsContext.Provider
+        value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+        }}
+      >
+        {showSettings ? <Settings /> : <PomodoroTimer />}
+      </SettingsContext.Provider>
+    </main>
   );
 }
