e482699e8dc10ac4fc97202a223efbb5eaf4937f
Shantanu993 <shantanugahtori4@gmail.com>
2024-04-01 13:38:24 +0530

    fix history heading not visible

--- components/HistoryPanel.js
+++ components/HistoryPanel.js
@@ -3,7 +3,7 @@ import React from "react";
 const HistoryPanel = ({ history }) => {
   return (
     <div className="mt-6">
-      <h2 className="text-2xl font-semibold mb-2">History</h2>
+      <h2 className="text-2xl font-semibold mb-2 text-gray-700">History</h2>
       <ul className="list-decimal list-inside bg-white p-4 rounded-lg shadow-lg">
         {history.map((item, index) => (
           <li key={index} className="text-sm text-gray-700 mb-1">
