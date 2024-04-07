20888519ec11669d9c0ce8da936d847dbac9b530
Shantanu993 <shantanugahtori4@gmail.com>
2024-04-07 11:37:43 +0530

    Update session history title

--- components/HistoryPanel.js
+++ components/HistoryPanel.js
@@ -6,7 +6,7 @@ const HistoryPanel = ({ history, session }) => {
       <h2 className="text-2xl font-semibold text-gray-700">
         Sessions Completed: {session}
       </h2>
-      <h2 className="text-2xl font-semibold text-gray-700">History</h2>
+      <h2 className="text-2xl font-semibold text-gray-700">Session History:</h2>
       <ul className="list-decimal list-inside bg-white p-4 rounded-lg shadow-lg">
         {history.map((item, index) => (
           <li key={index} className="text-sm text-gray-700 mb-1">
